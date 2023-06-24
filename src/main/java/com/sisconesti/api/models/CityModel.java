005278a411874d2c268536e40ff96891dd4455c0
muriloabreu <murilo.lilo@hotmail.com>
2023-06-24 16:27:44 -0300

    Implementação de classe CityModel atributos

--- src/main/java/com/sisconesti/api/models/CityModel.java
+++ src/main/java/com/sisconesti/api/models/CityModel.java
@@ -1,12 +1,17 @@
 package com.sisconesti.api.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "TB_CITYS")
 public class CityModel {
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 
 }
