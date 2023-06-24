3301e1b2a86339a69925334c7ae017f7c4b00469
muriloabreu <murilo.lilo@hotmail.com>
2023-06-24 16:23:23 -0300

    Criação de package Models e classe City

---
+++ src/main/java/com/sisconesti/api/models/CityModel.java
@@ -0,0 +1,12 @@
+package com.sisconesti.api.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_CITYS")
+public class CityModel {
+	
+	
+
+}
