fb501b1201ed5d4cf2271aae6e723e507a1d749c
muriloabreu <murilo.lilo@hotmail.com>
2023-06-26 22:47:01 -0300

    Criação de Package Services e Interface StateService

---
+++ src/main/java/com/sisconesti/api/services/StateService.java
@@ -0,0 +1,16 @@
+package com.sisconesti.api.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.sisconesti.api.models.StateModel;
+
+public interface StateService {
+	
+	List<StateModel> findAll();
+	Optional<StateModel> findById(Long id);
+	StateModel save(StateModel state);
+	void delete(StateModel state);
+	boolean existsByName(String name);
+
+}
