4935386b26db400ceb1c3fbf12e9c8f2b8f9b13d
muriloabreu <murilo.lilo@hotmail.com>
2023-06-26 23:15:04 -0300

    Criação da Classe CityServiceImpl

---
+++ src/main/java/com/sisconesti/api/services/CityServiceImpl.java
@@ -0,0 +1,54 @@
+package com.sisconesti.api.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sisconesti.api.models.CityModel;
+import com.sisconesti.api.repositories.CityRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class CityServiceImpl implements CityService{
+	
+	@Autowired
+	CityRepository cityRepository;
+
+	@Override
+	public List<CityModel> findByAll() {
+		
+		return cityRepository.findAll();
+	}
+
+	@Override
+	public Optional<CityModel> findById(Long id) {
+		
+		return cityRepository.findById(id);
+	}
+
+	@Override
+	@Transactional
+	public CityModel save(CityModel city) {
+		
+		return cityRepository.save(city);
+	}
+
+	@Override
+	@Transactional
+	public void delete(CityModel city) {
+		cityRepository.delete(city);
+		
+	}
+
+	@Override
+	public boolean existsByCityAndState(String nameState, String nameCity) {
+		
+		return cityRepository.existsByCityAndState(nameState, nameCity);
+	}
+	
+	
+
+}
