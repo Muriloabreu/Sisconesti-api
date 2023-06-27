a2b46c415aa4c1b83c99bd4acbeb93a51d2207b6
muriloabreu <murilo.lilo@hotmail.com>
2023-06-26 22:40:34 -0300

    Criação de Package Repositories e Interface StateRepository

---
+++ src/main/java/com/sisconesti/api/repositories/StateRepository.java
@@ -0,0 +1,10 @@
+package com.sisconesti.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sisconesti.api.models.StateModel;
+
+public interface StateRepository extends JpaRepository<StateModel, Long>{
+
+	boolean existsByName(String name);
+}
