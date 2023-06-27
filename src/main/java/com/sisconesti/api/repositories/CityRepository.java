58ab04afe64dc3b65d551b92a8e66b563aed165b
muriloabreu <murilo.lilo@hotmail.com>
2023-06-26 22:58:01 -0300

    Criação da Interface CityRepository

---
+++ src/main/java/com/sisconesti/api/repositories/CityRepository.java
@@ -0,0 +1,10 @@
+package com.sisconesti.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sisconesti.api.models.CityModel;
+
+public interface CityRepository extends JpaRepository<CityModel, Long>{
+
+	boolean existsByCityAndState(String nameState, String nameCity);
+}
