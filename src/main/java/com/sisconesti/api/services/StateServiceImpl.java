0885b45bd2615905d11566e5c70f6415e3124947
muriloabreu <murilo.lilo@hotmail.com>
2023-06-28 22:55:27 -0300

    Mudança de nome de parametro nameState

--- src/main/java/com/sisconesti/api/services/StateServiceImpl.java
+++ src/main/java/com/sisconesti/api/services/StateServiceImpl.java
@@ -44,9 +44,9 @@ public class StateServiceImpl implements StateService{
 	}
 
 	@Override
-	public boolean existsByName(String name) {
+	public boolean existsByName(String nameState) {
 		
-		return stateRepository.existsByName(name);
+		return stateRepository.existsByName(nameState);
 	}
 
 }
