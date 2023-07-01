296bd0abbb9fac93d7cbaec1c64163369c6e0d43
muriloabreu <murilo.lilo@hotmail.com>
2023-06-30 22:49:41 -0300

    Implementação de Método existsByName na interface CityRepository

--- src/main/java/com/sisconesti/api/repositories/CityRepository.java
+++ src/main/java/com/sisconesti/api/repositories/CityRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.sisconesti.api.models.CityModel;
 
 public interface CityRepository extends JpaRepository<CityModel, Long>{
-
+	
 	boolean existsByName(String name);
-}
+
+}	
