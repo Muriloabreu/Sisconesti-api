ab970f22ff88a0f68fb3eb157e59cf5a214f4f71
muriloabreu <murilo.lilo@hotmail.com>
2023-07-01 21:28:07 -0300

    Criação de Método buscar por nome na classe StateRepository Query customizada

--- src/main/java/com/sisconesti/api/repositories/StateRepository.java
+++ src/main/java/com/sisconesti/api/repositories/StateRepository.java
@@ -1,10 +1,16 @@
 package com.sisconesti.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.sisconesti.api.models.StateModel;
 
 public interface StateRepository extends JpaRepository<StateModel, Long>{
 
 	boolean existsByNameState(String nameState);
+	
+	@Query(value = "select s from StateModel s where s.name like %?1%" )
+	public List<StateModel> seacheByName(String name);
 }
