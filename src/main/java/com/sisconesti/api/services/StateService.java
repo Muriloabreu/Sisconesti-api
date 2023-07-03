90b3b36819a063c30a4cdef883151cc6d3c24a7f
muriloabreu <murilo.lilo@hotmail.com>
2023-07-02 21:16:20 -0300

    Implementação de métodos customizado utilizando @Query, Método> seacheByName

--- src/main/java/com/sisconesti/api/services/StateService.java
+++ src/main/java/com/sisconesti/api/services/StateService.java
@@ -4,13 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import com.sisconesti.api.models.StateModel;
+import com.sisconesti.api.projections.StateJoinMinProjection;
 
 public interface StateService {
 	
 	List<StateModel> findAll();
+	List<StateJoinMinProjection> seacheByName(String name);
 	Optional<StateModel> findById(Long id);
 	StateModel save(StateModel state);
 	void delete(StateModel state);
 	boolean existsByNameState(String nameState);
+	
 
 }
