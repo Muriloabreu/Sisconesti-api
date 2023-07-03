4e4cc4522abe8331957cc98c74394445556e3689
muriloabreu <murilo.lilo@hotmail.com>
2023-07-02 21:15:22 -0300

    Implementação de métodos customizado utilizando @Query

--- src/main/java/com/sisconesti/api/repositories/StateRepository.java
+++ src/main/java/com/sisconesti/api/repositories/StateRepository.java
@@ -6,11 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.sisconesti.api.models.StateModel;
+import com.sisconesti.api.projections.StateJoinMinProjection;
 
 public interface StateRepository extends JpaRepository<StateModel, Long>{
 
 	boolean existsByNameState(String nameState);
 	
 	@Query(value = "select s from StateModel s where s.name like %?1%" )
-	public List<StateModel> seacheByName(String name);
+	public List<StateJoinMinProjection> seacheByName(String name);
 }
