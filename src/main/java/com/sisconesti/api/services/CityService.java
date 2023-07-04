56fcfbfbb583c947a5b832148963885bb2b36a0a
muriloabreu <murilo.lilo@hotmail.com>
2023-07-03 23:20:53 -0300

    Criação de método customizado na interface CityService, para buscar City por nome

--- src/main/java/com/sisconesti/api/services/CityService.java
+++ src/main/java/com/sisconesti/api/services/CityService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import com.sisconesti.api.models.CityModel;
+import com.sisconesti.api.projections.CityJoinMinProjection;
+import com.sisconesti.api.projections.StateJoinMinProjection;
 
 public interface CityService {
 	
 	List<CityModel> findAll();
+	List<CityJoinMinProjection> seacheByName(String name);
 	Optional<CityModel> findById(Long id);
 	CityModel save(CityModel city);
 	void delete(CityModel city);
