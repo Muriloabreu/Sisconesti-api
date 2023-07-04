2dae445f7889ab34ec2a88bb6cca0fa60f59e930
muriloabreu <murilo.lilo@hotmail.com>
2023-07-03 23:19:56 -0300

    Criação de método customizado usando @Query, para buscar por nome

--- src/main/java/com/sisconesti/api/repositories/CityRepository.java
+++ src/main/java/com/sisconesti/api/repositories/CityRepository.java
@@ -1,11 +1,19 @@
 package com.sisconesti.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.sisconesti.api.models.CityModel;
+import com.sisconesti.api.projections.CityJoinMinProjection;
+import com.sisconesti.api.projections.StateJoinMinProjection;
 
 public interface CityRepository extends JpaRepository<CityModel, Long>{
 	
 	boolean existsByName(String name);
+	
+	@Query(value = "select c from CityModel c where c.name, c.state like %?1% ")
+	public List<CityJoinMinProjection> seacheByName(String name);
 
 }	
