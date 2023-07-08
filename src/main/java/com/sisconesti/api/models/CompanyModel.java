722a90ca06ea4547f1800c7d4bbac10d18fb2cbb
muriloabreu <murilo.lilo@hotmail.com>
2023-07-07 22:42:02 -0300

    Implementação e remoção de atributos na classe Moel CompanyModel

--- src/main/java/com/sisconesti/api/models/CompanyModel.java
+++ src/main/java/com/sisconesti/api/models/CompanyModel.java
@@ -1,5 +1,6 @@
 package com.sisconesti.api.models;
 
+import java.util.List;
 import java.util.Objects;
 
 
@@ -9,6 +10,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,9 @@ public class CompanyModel {
 	private String corporate_name;	
 	@Column(nullable = false)
 	private String fantasy_name;
+	@OneToMany
+	@JoinColumn(name = "company_id")
+	private List<BranchModel> branchs;
 	
 	
 	/* Constructor */
