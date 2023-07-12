bbe27b9138f6acb88489a05e776c18534534ff82
muriloabreu <murilo.lilo@hotmail.com>
2023-07-11 22:10:37 -0300

    Implementação / add de atributos na classe Role

--- src/main/java/com/sisconesti/api/models/Role.java
+++ src/main/java/com/sisconesti/api/models/Role.java
@@ -1,6 +1,44 @@
 package com.sisconesti.api.models;
 
+import org.springframework.security.core.GrantedAuthority;
 
-public class Role {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_ROLE")
+public class Role implements GrantedAuthority{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String nameRole; /*PAPEL, ROLE_GERENTE, ROLE_ADM*/
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNameRole() {
+		return nameRole;
+	}
+
+	public void setNameRole(String nameRole) {
+		this.nameRole = nameRole;
+	}
+
+	@Override
+	public String getAuthority() {
+		return this.nameRole;
+	}
+	
+	
 
 }
