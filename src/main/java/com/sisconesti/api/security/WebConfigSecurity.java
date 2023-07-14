fecd850f7b9d085e40bfe1d10d444b1c610fed00
muriloabreu <murilo.lilo@hotmail.com>
2023-07-13 21:48:07 -0300

    Add de Package Securoty com classe WebConfigSecurity

---
+++ src/main/java/com/sisconesti/api/security/WebConfigSecurity.java
@@ -0,0 +1,13 @@
+package com.sisconesti.api.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/*Mapeia URL, endereços, autoriza ou bloqueia acesso a URL*/
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
+	
+	
+
+}
