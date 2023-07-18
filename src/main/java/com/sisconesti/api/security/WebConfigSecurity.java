cff0b6a675da094d04a871bfb591a4989de5e5d2
muriloabreu <murilo.lilo@hotmail.com>
2023-07-17 22:24:57 -0300

    Implementação da classe WebConfigSecurity, com métodos para autenticação

--- src/main/java/com/sisconesti/api/security/WebConfigSecurity.java
+++ src/main/java/com/sisconesti/api/security/WebConfigSecurity.java
@@ -1,13 +1,58 @@
 package com.sisconesti.api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.sisconesti.api.services.UserServiceImpl;
+
 
 /*Mapeia URL, endereços, autoriza ou bloqueia acesso a URL*/
 @Configuration
 @EnableWebSecurity
-public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
+public class WebConfigSecurity extends WebSecurityConfiguration {
 	
+	@Autowired
+	private UserServiceImpl userServiceImpl;
 	
-
+	/*CONFIGURA AS SOLICITAÇÕES DE AACESSO POR HTTP*/
+	private void configure(HttpSecurity http) throws Exception {
+		
+		/*ATIVANDO PROTEÇÃO CONTRA USUARIOS QUE NÃO ESTÃO VALIDADOS POR TOKEN*/
+		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+		
+		/*ATIVANDO A PERMISSÃO DE ACESSO A PAGINA INICIAL DO SISTEMA EX.: SISTEMA.COM.BR/INDEX.HTML*/
+		.disable().authorizeRequests().requestMatchers("/").permitAll()
+		.requestMatchers("/index").permitAll()
+		
+		/*URL LOGOUT - REDIRECIONA APÓS USER SAIR DO SISTEMA */
+		.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+		
+		/*MAPEIA URL DE LOGOUT E INVALIDA USUARIO*/
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		;
+		
+		/*FILTRA AS REQUISIÇÕES DE LOGIN PARA  AUTENTICAÇÃO*/
+		
+		
+		/*FILTRA DEMAIS REQUISIÇÕES PARA VERIFICAR A PRESENÇA DO TOKEN JWT NO HEADER HTTP */
+		
+		
+	}
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		
+		/*SERVICE QUE IRA CONSULTAR O USUARIO NO BANCO DE DADOS*/
+		auth.userDetailsService(userServiceImpl)
+		
+		/*PADRAO DE CODIFICAÇÃO DE SENHA*/
+		.passwordEncoder(new BCryptPasswordEncoder());
+		
+	}
 }
