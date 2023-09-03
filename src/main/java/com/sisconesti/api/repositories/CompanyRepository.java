67a6550014bf6b7a93d81b1a15e656815b2f0029
muriloabreu <murilo.lilo@hotmail.com>
2023-09-03 17:36:04 -0300

    Criação de método customizado om nome seacheByName na interface CompanyRepository

--- src/main/java/com/sisconesti/api/repositories/CompanyRepository.java
+++ src/main/java/com/sisconesti/api/repositories/CompanyRepository.java
@@ -1,6 +1,9 @@
 package com.sisconesti.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.sisconesti.api.models.CompanyModel;
 
@@ -10,5 +13,7 @@ public interface CompanyRepository extends JpaRepository<CompanyModel, Long> {
 	
 	boolean existsByCorporateName(String name);
 	boolean existsByFantasyName(String name);
+	@Query(value = "SELECT c.corporateName, c.fantasyName from CompanyModel c WHERE c.fantasyName LIKE %?1%")
+	List<CompanyModel>seacheByName(String name);
 
 }
