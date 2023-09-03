733816436a179653c637cfd197749f0d8e0b5418
muriloabreu <murilo.lilo@hotmail.com>
2023-09-03 17:31:01 -0300

    Implementação de metodo de teste save para a classe Company

--- src/test/java/com/sisconesti/api/serviceTest/CompanyTest.java
+++ src/test/java/com/sisconesti/api/serviceTest/CompanyTest.java
@@ -1,6 +1,10 @@
 package com.sisconesti.api.serviceTest;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,12 +22,17 @@ public class CompanyTest {
 	@Autowired
 	CompanyService cs;
 	
-	@Ignore
+	@Test
+	//@Ignore
 	public void saveTest() {
 		
 		CompanyModel company1 = new CompanyModel();
 		company1.setCorporateName("Empresa 01 LMTD");
 		company1.setFantasyName("Farmacia de Todos");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+		
+		LocalDateTime dt1 = LocalDateTime.parse("04/09/2023 17:16:10", formatter );
+		company1.setRegistrationDate(dt1);
 		
 		cs.save(company1);
 	}
