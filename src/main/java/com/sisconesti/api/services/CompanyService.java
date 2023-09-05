f1c43cb561b08c30276f13a6c921666218404320
muriloabreu <murilo.lilo@hotmail.com>
2023-09-04 22:47:59 -0300

    Criação de declaração de método seacheByName na interface CompanyService

--- src/main/java/com/sisconesti/api/services/CompanyService.java
+++ src/main/java/com/sisconesti/api/services/CompanyService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import com.sisconesti.api.models.CompanyModel;
+import com.sisconesti.api.projections.CompanyJoinMinProjection;
 
 public interface CompanyService {
 
 	List<CompanyModel> findAll();
+	List<CompanyJoinMinProjection>seacheByName(String name);
 	Optional<CompanyModel> findById(Long id);
 	CompanyModel save(CompanyModel company);
 	void delete (CompanyModel company);
