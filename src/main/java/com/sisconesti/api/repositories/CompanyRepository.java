95cbc072616cc4987dee71b75c4ac09d6c6aff44
muriloabreu <murilo.lilo@hotmail.com>
2023-09-04 22:46:32 -0300

    Correção de LIST<> no método seacheByNameCompany na interface CompanyRepository

--- src/main/java/com/sisconesti/api/repositories/CompanyRepository.java
+++ src/main/java/com/sisconesti/api/repositories/CompanyRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.sisconesti.api.models.CompanyModel;
+import com.sisconesti.api.projections.CompanyJoinMinProjection;
 
 
 public interface CompanyRepository extends JpaRepository<CompanyModel, Long> {
@@ -14,6 +15,6 @@ public interface CompanyRepository extends JpaRepository<CompanyModel, Long> {
 	boolean existsByCorporateName(String name);
 	boolean existsByFantasyName(String name);
 	@Query(nativeQuery = true, value = "SELECT corporate_name, fantasy_name FROM tb_company WHERE fantasy_name LIKE %?1%")
-	List<CompanyModel>seacheByNameCompany(String name);
+	List<CompanyJoinMinProjection> seacheByNameCompany(String name);
 
 }
