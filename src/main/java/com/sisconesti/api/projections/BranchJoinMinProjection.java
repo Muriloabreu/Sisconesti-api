f33837f496d8414577a5f3433c428bae094a630a
Muriloabreu <murilo.lilo@hotmail.com>
2024-06-03 22:24:37 -0300

    Criação de classe projecão para relatorios customizados

---
+++ src/main/java/com/sisconesti/api/projections/BranchJoinMinProjection.java
@@ -0,0 +1,13 @@
+package com.sisconesti.api.projections;
+
+public interface BranchJoinMinProjection {
+	
+	String getNameBranch();
+	String getCnpj();
+	String getNameCompany();
+	String getCityname();
+	String getNamestate();
+	String getUf_State();
+
+
+}
