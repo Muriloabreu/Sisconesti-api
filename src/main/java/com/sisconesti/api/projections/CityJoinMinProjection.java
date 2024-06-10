a828176673256bdad758cade5b8a0d0957aa2adc
Muriloabreu <murilo.lilo@hotmail.com>
2024-06-09 21:51:44 -0300

    Implementação na interface de projecao CityJoinMinProjection removendo o campo Date e mudando o campo nameState para Uf

--- src/main/java/com/sisconesti/api/projections/CityJoinMinProjection.java
+++ src/main/java/com/sisconesti/api/projections/CityJoinMinProjection.java
@@ -2,9 +2,9 @@ package com.sisconesti.api.projections;
 
 public interface CityJoinMinProjection {
 
-	String getDate();
+	
 	String getNameCity();
 	String getNameState();
-	String getSiglaState();
+	String getUf();
 
 }
