6f17e86b05fc5aab3344a46bc15f48caa61a4476
Muriloabreu <murilo.lilo@hotmail.com>
2024-06-09 21:11:27 -0300

    Criação de métodos de busca trazendo uma  List Branch

--- src/main/java/com/sisconesti/api/services/BranchService.java
+++ src/main/java/com/sisconesti/api/services/BranchService.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import com.sisconesti.api.models.BranchModel;
+import com.sisconesti.api.projections.BranchJoinAllProjection;
+import com.sisconesti.api.projections.BranchJoinMinProjection;
 
 public interface BranchService {
 	
 	List<BranchModel> findAll();
+	List<BranchJoinMinProjection>seacheByName(String name);
+	List<BranchJoinAllProjection>findAllById(Long id);
 	Optional<BranchModel> findById(Long id);
 	BranchModel save(BranchModel branch);
 	void delete (BranchModel brance);
