c6825829de39f9001f99069340b7ee2c35d96910
Muriloabreu <murilo.lilo@hotmail.com>
2024-07-30 22:16:05 -0300

    Criação da Classe EmployeeDto na Package Dtos

---
+++ src/main/java/com/sisconesti/api/dtos/EmployeeDto.java
@@ -0,0 +1,74 @@
+package com.sisconesti.api.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class EmployeeDto {
+	
+	@NotBlank
+	private String firstName;
+	@NotBlank
+	private String lastName;
+	@NotBlank
+	private String telephone;
+	@NotBlank
+	private String personType;
+	@NotBlank
+	private String email;
+	@NotBlank
+	private String matricula;
+	@NotBlank
+	private String cpf;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getPersonType() {
+		return personType;
+	}
+	public void setPersonType(String personType) {
+		this.personType = personType;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
